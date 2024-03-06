33964584d8311529bbe41f6275c89bd11f512e2d
Max Ast <maxast31@gmail.com>
2024-03-06 14:23:04 +0100

    create marquee with absolutely positioned duplicate

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,10 +5,24 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      animation: {
+        marquee: "marquee 10s linear infinite",
+        marquee2: "marquee2 10s linear infinite",
+      },
       fontFamily: {
         sans: ["Helvetica Neue", ...defaultTheme.fontFamily.sans],
         serif: ["Lusitana", ...defaultTheme.fontFamily.serif],
         display: ["Source Serif Pro", ...defaultTheme.fontFamily.serif],
+      },
+      keyframes: {
+        marquee: {
+          from: { transform: "translateX(0%)" },
+          to: { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          from: { transform: "translateX(100%)" },
+          to: { transform: "translateX(0%)" },
+        },
       },
     },
   },
