8b1bd1cd5c71619ff9bd444ce91b6b29e9f3ae5c
Max Ast <maxast31@gmail.com>
2024-11-22 15:37:50 +0100

    put bg-grid back in

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,8 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+const svgToDataUri = require("mini-svg-data-uri");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -26,5 +30,18 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    ({ matchUtilities, theme }) => {
+      matchUtilities(
+        {
+          "bg-grid": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 };
